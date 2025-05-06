1c3d094442486e3913fd4c36a3f552227096530a
Matyas Seeuws <116265035+NannyPig@users.noreply.github.com>
2025-05-06 15:58:14 +0200

    Update vite.config.ts

--- react-bios/vite.config.ts
+++ react-bios/vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/RUBweb/',  // Voeg dit toe om ervoor te zorgen dat de paden correct zijn
   plugins: [react(), tailwindcss()],
 })
